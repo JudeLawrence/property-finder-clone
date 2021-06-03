507d5109bf8fb887e403ec53cf65df2bbb518c18
JudeLawrence <judesmail@gmail.com>
2021-06-03 16:14:34 +0400

    Add image hosting to properties

--- app/models/property_listing.rb
+++ app/models/property_listing.rb
@@ -1,6 +1,6 @@
 class PropertyListing < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_one_attached :image
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?

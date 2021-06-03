ceb75e9edcba0e7f2d24c23c5260aa267964af4c
Yusmida Arien <yusarien@gmail.com>
2021-06-03 14:24:29 +0400

    form  creation

--- app/controllers/property_listings_controller.rb
+++ app/controllers/property_listings_controller.rb
@@ -10,6 +10,22 @@ class PropertyListingsController < ApplicationController
   def show
   end
 
+  def new
+    @property_listing = PropertyListing.new
+    authorize @property_listing
+  end
+
+  def create
+    @property_listing = PropertyListing.new(property_listing_params)
+    @property_listing.user = current_user
+    authorize @property_listing
+    if @property_listing.save
+      redirect_to property_listing_path(@property_listing)
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def set_listing
@@ -17,4 +33,10 @@ class PropertyListingsController < ApplicationController
     authorize @property
   end
 
+  def property_listing_params
+    params.require(:property_listing).permit(:title, :sub_title, :description,
+                                             :location, :property_type, :listing_price, :number_of_bedrooms,
+                                             :number_of_bathrooms, :number_of_parking_spaces, :property_size,
+                                             :amenities, :year_built)
+  end
 end

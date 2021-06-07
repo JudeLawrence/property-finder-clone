e1b227f4d4d15859edfbf362d570ea7bcefa3cc1
JudeLawrence <judesmail@gmail.com>
2021-06-07 15:54:31 +0400

    Added function to accept or decline offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,17 +8,30 @@ class OffersController < ApplicationController
   def create
     @offer = Offer.new(offer_params)
     @offer.user = current_user
-    @offer.property_listing = PropertyListing.find(params[:property_listing_id])
+    @property_listing = PropertyListing.find(params[:property_listing_id])
+    @offer.property_listing = @property_listing
     if @offer.save
-      redirect_to property_listings_path
+      redirect_to @property_listing, notice: "Your offer has been submitted!"
     else
-      # Do something if not validated
+      render :new
     end
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    @offer.status = params["status"]
+    @offer.save
+    redirect_to @offer
   end
 
   private
 
   def offer_params
-    params.require(:offer).permit(:offer_amount, :buyer_comment)
+    params.require(:offer).permit(:offer_amount, :buyer_comment, :status, :seller_comment)
   end
+
 end
